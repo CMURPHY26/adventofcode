4ab42ec4956d04f735d7650dda2180895d21c833
Chris <28192055+CMURPHY26@users.noreply.github.com>
2022-12-07 20:26:27 -0500

    part2

--- day1/day1-calorie-counting.mjs
+++ day1/day1-calorie-counting.mjs
@@ -5,9 +5,15 @@ const elves = input.split('\n\n');
 
 let totalCalories = 0;
 
+const elvesTotalsArray = [];
+
 for(const elf of elves) {
     const elfTotal = elf.split('\n').reduce((total, calories) => total + Number(calories), 0);
     totalCalories = Math.max(totalCalories, elfTotal);
+    elvesTotalsArray.push(elfTotal);
 }
 
+elvesTotalsArray.sort((a,b) => b - a);
+
 console.log(totalCalories);
+console.log(elvesTotalsArray.slice(0,3).reduce((a,b) => a + b));
